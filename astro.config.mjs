ad353f5e048b46da706c6fbb0a71aa8d077fc8b4
IgnacioThomasBenitez <benitezignacio46@gmail.com>
2025-10-27 19:23:21 -0300

    h

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/static'; // ← esta es la clave
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  output: 'static', // ← indicamos que es un sitio estático
   adapter: vercel(),
 });
